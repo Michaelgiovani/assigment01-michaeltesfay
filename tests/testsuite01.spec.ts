56b260e8808833cf639fdd024cb4e5354b2e206a
Michael Tesfay <Michaeleygiovani@gmail.com>
2024-09-11 22:04:31 +0200

    added more tests

--- tests/testsuite01.spec.ts
+++ tests/testsuite01.spec.ts
@@ -2,33 +2,77 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from './pages/login-page';
 import { DashboardPage } from './pages/dashboard-page';
 import { ClientPage } from './pages/client-page';
-        
+import { CreateRoomPage } from './pages/room-page';
+import { CreateBillPage } from './pages/bill-page';
+import { CreateReservationPage } from './pages/reservation-page';
+import { faker } from '@faker-js/faker';
+
+
+test.beforeEach(async ({ page }) => {
+  const loginpage = new LoginPage(page);
+  const dashboardpage = new DashboardPage(page);
+
+  await loginpage.goto();
+  await loginpage.performLogin(process.env.TEST_USERNAME!, process.env.TEST_PASSWORD!);
+  await page.waitForTimeout(10000);
+  await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+});
+
 test.describe('Test suite 01', () => {
-    test('Test case 01', async ({ page }) => {
-      const loginpage = new LoginPage(page);
-      const dashboardpage = new DashboardPage(page);
+  test('Test case 01', async ({ page }) => {
+    const dashboardpage = new DashboardPage(page);
+    await dashboardpage.performLogout();
+  });
+
+  test('Test case 02', async ({ page }) => {
+    const dashboardpage = new DashboardPage(page);
+    const clientpage = new ClientPage(page);
+
+    const fullName = faker.person.fullName();
+    const userEmail = faker.internet.email();
+    const userPhoneNo = faker.phone.number();
+
+    await dashboardpage.navigateToClients();
+    await expect(page.getByRole('heading', { name: 'New Client' })).toBeVisible();
+    await clientpage.createClient(fullName, userEmail, userPhoneNo);                                                                 
+    await clientpage.verifyClientDetails(fullName, userEmail, userPhoneNo);
+    await expect(page).toHaveURL('http://localhost:3000/clients');
+
+  });
+
+  test('Test case 03', async ({ page }) => {
+    const dashboardpage = new DashboardPage(page);
+    const createRoomPage = new CreateRoomPage(page);
+
+    await dashboardpage.navigateToRooms();
+    await expect(page.getByRole('heading', { name: 'New Room' })).toBeVisible();
   
-      await loginpage.goto();
-      await loginpage.performLogin(process.env.TEST_USERNAME!, process.env.TEST_PASSWORD!);
-      await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
-      await dashboardpage.performLogout();
+    await createRoomPage.fillOutCreateRoomsForm();
+    await expect(page).toHaveURL('http://localhost:3000/rooms');
+  });
   
-      //await page.waitForTimeout(5000);
-    });
+  test('Test case 04', async ({ page }) => {
+    const dashboardpage = new DashboardPage(page);
+    const createBillPage = new CreateBillPage(page);
+
+    await dashboardpage.navigateToBills();
+    await page.waitForTimeout(10000);
+    await expect(page.getByRole('heading', { name: 'New Bill' })).toBeVisible();
   
-    test('Test case 02', async ({ page }) => {
-      const loginpage = new LoginPage(page);
-      const dashboardpage = new DashboardPage(page);
-      const clientpage = new ClientPage(page);
+    await createBillPage.fillOutCreateBillsForm();
+    await expect(page).toHaveURL('http://localhost:3000/bills');
+  });
+
+  test('Test case 05', async ({ page }) => {
+    const dashboardpage = new DashboardPage(page);
+    const createReservationPage = new CreateReservationPage(page);
+
+    await dashboardpage.navigateToReservations();
+    await page.waitForTimeout(5000);
+    await expect(page.getByRole('heading', { name: 'New Reservation' })).toBeVisible();
   
-      await loginpage.goto();
-      await loginpage.performLogin(process.env.TEST_USERNAME!, process.env.TEST_PASSWORD!);
-      await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
-      await page.waitForTimeout(5000);
-      await dashboardpage.navigateToClients();
-      await expect(page.getByRole('heading', { name: 'New Client' })).toBeVisible();
-    
-      const fullName
-    
-    });
+    await createReservationPage.fillOutCreateReservationsForm();
+    await expect(page).toHaveURL('http://localhost:3000/reservations');
+  });
+  
 });
